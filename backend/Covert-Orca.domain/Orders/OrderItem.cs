fd006f2f37ee45f8811781b87e750f42093bf7aa
Tom Huston <tomjh704@gmail.com>
2021-04-13 22:37:10 -0400

    Minor update.

--- backend/Covert-Orca.domain/Orders/OrderItem.cs
+++ backend/Covert-Orca.domain/Orders/OrderItem.cs
@@ -5,7 +5,7 @@ namespace Covert_Orca.domain.Orders
     public class OrderItem
     {
         public int Id { get; set; }
-        public Item Item { get; get; }
+        public Item Item { get; set; }
         public int Quantity { get; set; }
         public decimal Price => Item.Price * Quantity;
     }
