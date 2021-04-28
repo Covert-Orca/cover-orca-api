3c83a835ae3c551f2939e63d97da50b2b1a92553
Tom Huston <tomjh704@gmail.com>
2021-04-27 21:50:24 -0400

    Lab 6: Part 1

---
+++ backend/covert-orca-api.Domain.Tests/RatingTests.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace covert_orca_api.Domain.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        [ExpectedException(typeof(ArgumentException))]
+        public void Create_With_Bad_Start_Throws_Error()
+        {
+            var rating = new Rating(0, "Name", "Review");
+        }
+        public void Can_Create_New_Rating()
+        {
+            //Arrange
+            var rating = new Rating(1, "Mike", "Great fit!");
+
+            //Act - skip because there isn't action on constructor
+
+            //Assert
+            Assert.AreEqual(1, rating.Star);
+            Assert.AreEqual("Mike", rating.UserName);
+            Assert.AreEqual("Great fit!", rating.Review);
+        }
+    }
+}
